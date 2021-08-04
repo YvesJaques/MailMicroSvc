2f556e87cb9c2fa01785a164af2b869880658bc3
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-08-04 13:51:14 -0300

    feat: Import Contacts Service

---
+++ src/services/ImportContactsService.spec.ts
@@ -0,0 +1,72 @@
+import { Readable } from 'stream';
+
+import mongoose from 'mongoose';
+
+import Contact from '../../src/schemas/Contact';
+import Tag from '../../src/schemas/Tag';
+import ImportContactsService from './ImportContactsService';
+
+describe('Import', () =>{
+    beforeAll(async () => {
+        if (!process.env.MONGO_URL) {
+            throw new Error('MongoDB server not initialized');
+        }
+
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+    });
+
+    afterAll(async () => {
+        mongoose.connection.close();
+    });
+
+    beforeEach(async () => {
+        await Contact.deleteMany({});
+        await Tag.deleteMany({})
+    });
+
+    it('Should be able to import new contacts', async () => {
+        const contactsFileStream = Readable.from([
+            'teste1@teste.com\n',
+            'teste2@teste.com\n',
+            'teste3@teste.com\n',
+        ])
+
+        const importContacts = new ImportContactsService();
+
+        await importContacts.run(contactsFileStream, ['Students', 'Class A']);
+
+        const createdTags = await Tag.find({}).lean();
+
+        expect(createdTags).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ title: 'Students' }),
+                expect.objectContaining({ title: 'Class A' }),
+            ]),
+        );
+
+        const createdTagsIds = createdTags.map(tag => tag._id);
+
+        const createdContacts = await Contact.find({}).lean();
+
+        expect(createdContacts).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    email: 'teste1@teste.com',
+                    tags: createdTagsIds,
+                }),
+                expect.objectContaining({
+                    email: 'teste2@teste.com',
+                    tags: createdTagsIds,
+                }),
+                expect.objectContaining({
+                    email: 'teste3@teste.com',
+                    tags: createdTagsIds,
+                }),
+            ]),
+        );
+    });
+})
---
+++ src/services/ImportContactsService.ts
@@ -0,0 +1,32 @@
+import { Readable } from 'stream';
+import csvParse from 'csv-parse';
+
+import Tag from '@schemas/Tag';
+import Contact from '@schemas/Contact';
+
+class ImportContactsService {
+    async run(contactsFileStream: Readable, tags: string[]): Promise<void> {
+        const parser = csvParse({
+            delimiter: ';',
+        });
+
+        const parseCSV = contactsFileStream.pipe(parser);
+
+        const tagsData = tags.map(tag => ({
+            title: tag,
+        }));
+
+        const createdTags = await Tag.create(tagsData);
+        const tagsIds = createdTags.map(tag => tag._id);
+
+        parseCSV.on('data', async line => {
+            const [email] = line;
+
+            await Contact.create({ email, tags: tagsIds });
+        });
+
+        await new Promise(resolve => parseCSV.on('end', resolve));
+    }
+}
+
+export default ImportContactsService;
